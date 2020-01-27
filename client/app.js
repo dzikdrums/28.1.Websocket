bb0c7c130074a6d604777b29fc39b4178b13c280
Maciej Dzik <dzikdrums@gmail.com>
2020-01-27 14:17:54 +0100

    add chat functionalities

--- client/app.js
+++ client/app.js
@@ -1,3 +1,5 @@
+const socket = io();
+
 var userName = '';
 const loginForm = document.getElementById('welcome-form');
 const messagesSection = document.getElementById('messages-section');
@@ -6,19 +8,21 @@ const addMessageForm = document.getElementById('add-messages-form');
 const userNameInput = document.getElementById('username');
 const messageContentInput = document.getElementById('message-content');
 
+socket.on('message', ({ author, content }) => addMessage(author, content))
+
 addMessage = (author, content) => {
   const message = document.createElement('ul');
   message.classList.add('message');
   message.classList.add('message--received')
 
   if(author === userName) message.classList.add('message--self');
+  
   message.innerHTML = `
     <h3 class="message__author">${userName === author ? 'You' : author }</h3>
     <div class="message__content">
-      ${content}
+    ${author === "Chat Bot" ? `<i>${content}</>` : content}  
     </div>
   `;
-
   messagesList.appendChild(message);
 };
 
@@ -27,11 +31,12 @@ sendMessage = (e) => {
 
   if(messageContentInput.value) {
     addMessage(userName, messageContentInput.value);
+    socket.emit('message', { author: userName, content: messageContentInput.value });
+    addMessageForm.reset();
   }
   else {
     alert('Input cannot be left blank!')
   }
-  addMessageForm.reset();
 
 };
 
@@ -44,6 +49,7 @@ login = (e) => {
 
     loginForm.classList.remove('show');
     messagesSection.classList.add('show');
+    socket.emit('login', { name: userName });
   }
   else {
     alert('You can\'t leave the login input empty!');
--- server.js
+++ server.js
@@ -1,13 +1,62 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const socket = require('socket.io');
 
 app.use(express.static(path.join(__dirname, '/client')));
 
 const messages = [];
+const users = [];
 
 app.get('*', (req, res) => {
   res.render('./client/index.html');
 });
 
-app.listen(8000);
+const server = app.listen(8000);
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+  socket.on('message', () => { console.log('Oh, I\'ve got something from ' + socket.id) });
+
+  socket.on('message', (message) => {
+    console.log('Oh, I\'ve got something from ' + socket.id);
+    messages.push(message);
+    socket.broadcast.emit('message', message);
+  });
+
+  socket.on('login', (user) => {
+    console.log('Oh, I\'ve got a new user logged in - ' + user.name);
+    const message = ({ author: 'Chat Bot', content: user.name + ' has joined the conversation!'})
+    
+    messages.push(message);
+    socket.broadcast.emit('message', message);
+
+    const newUser = {
+      name: user.name,
+      id: socket.id,
+    };
+
+    users.push(newUser);
+  });
+
+  socket.on('disconnect', () => {
+    let exitingUser = '';
+
+    const deleteUser = (user, index) => {
+      if(user.id === socket.id) {
+        users.splice(index, 1)
+        exitingUser = user.name;
+      };
+    };
+
+    users.some(deleteUser);
+    const message = ({ author: 'Chat Bot', content: exitingUser + ' has left the conversation... :('});
+    socket.broadcast.emit('message', message);
+    console.log('Oh, socket ' + socket.id + ' has left')
+  });
+
+
+});
+
